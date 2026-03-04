755b8b12178aceebe9e94d840e62ef3eeb716f56
Mannybranch <78517183+Mannybranch@users.noreply.github.com>
2026-03-04 15:47:27 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,6 @@ import ProductCards from "./components/ProductCards";
 export default function Home() {
   return (
     <main>
-      <h1>Hello, World!</h1>
-      <Link href="/users">Go to Users Page</Link>
-      <ProductCards />
     </main>
   );
 }
