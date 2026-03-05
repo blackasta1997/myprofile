71b5ae189ed515ebd02c6d94db7daafe6264232c
Mannybranch <78517183+Mannybranch@users.noreply.github.com>
2026-03-05 11:54:58 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,15 @@ import data from "./data/data";
 
 export default async function Home() {
   const testContent = await data();
-  console.log(testContent);
   return (
     <main>
-      {process.env.CONTENTFUL_SPACE_ID}
+      <div className="container">
+        {testContent.map((content) => (
+          <div key={content.sys.id}>
+            <h2>{String(content.fields.title)}</h2>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
