95a9b820be194b191794bf6f04192e81d9b2f340
Mannybranch <78517183+Mannybranch@users.noreply.github.com>
2026-03-05 10:08:20 +0800

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@ import data from "./data/data";
 
 export default async function Home() {
   const testContent = await data();
-  console.log(testContent);
   return (
     <main>
+      <div className="container">
+        {testContent.map((content) => (
+          <div key={content.sys.id}>
+            <h2>{content.fields.title}</h2>
+            <p>{content.fields.description}</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
