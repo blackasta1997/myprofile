787077617afb1705d73a16cae0651b4e31a3d0e1
Mannybranch <78517183+justMeManny@users.noreply.github.com>
2026-03-06 17:46:59 +0800

    Small Features: Landing Page

--- app/components/display/smallFeature.tsx
+++ app/components/display/smallFeature.tsx
@@ -31,9 +31,7 @@ const SmallFeature = () => {
     <section ref={sectionRef} className="py-16 lg:py-24 bg-white">
       <div className="max-w-7xl mx-auto px-6 sm:px-12 lg:px-16">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div className={`grid grid-flow-col auto-col-max transition-all duration-700 transform ${
-            isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
-          }`}>
+          <div className={`grid grid-flow-col auto-col-max transition-all duration-700 transform ${ isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10' }`}>
             <div className=" w-16 h-16 left-0">
               <svg fill="#155dfc" width="64" height="64" viewBox="0 0 1.92 1.92" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" version="1.1" transform="scale(-1 1)">
                 <path d="M1.52 0.64a0.239 0.239 0 0 0 -0.08 0.465v0.135a0.36 0.36 0 0 1 -0.72 0V1.193a0.48 0.48 0 0 0 0.4 -0.473V0.24a0.08 0.08 0 0 0 -0.08 -0.08h-0.16a0.08 0.08 0 0 0 0 0.16h0.08v0.4a0.32 0.32 0 0 1 -0.64 0V0.32h0.08a0.08 0.08 0 0 0 0 -0.16H0.24a0.08 0.08 0 0 0 -0.08 0.08v0.48A0.48 0.48 0 0 0 0.56 1.193v0.047a0.52 0.52 0 0 0 1.04 0v-0.135A0.239 0.239 0 0 0 1.52 0.64m0 0.32a0.08 0.08 0 1 1 0.08 -0.08 0.08 0.08 0 0 1 -0.08 0.08" />
@@ -44,9 +42,7 @@ const SmallFeature = () => {
               <p className="text-gray-600 text-sm leading-relaxed">Family medicine & family to medicine.</p>
             </div>
           </div>
-          <div className={`grid grid-flow-col auto-col-max transition-all duration-700 transform delay-150 ${
-            isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
-          }`}>
+          <div className={`grid grid-flow-col auto-col-max transition-all duration-700 transform delay-150 ${ isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10' }`}>
             <div className="w-16 h-16 flex justify-center">
               <svg fill="#155dfc" width="64px" height="64px" viewBox="0 0 1.92 1.92" xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24">
                 <path d="M1.568 0.288c-0.016 -0.016 -0.04 -0.024 -0.064 -0.016 -0.176 0.04 -0.352 0 -0.496 -0.104 -0.024 -0.016 -0.064 -0.016 -0.088 0 -0.152 0.104 -0.328 0.144 -0.504 0.104 -0.04 -0.008 -0.088 0.024 -0.096 0.064v0.616c0 0.232 0.112 0.448 0.304 0.584l0.296 0.208c0.024 0.016 0.064 0.016 0.096 0l0.296 -0.208c0.192 -0.136 0.304 -0.352 0.304 -0.584V0.352c-0.016 -0.024 -0.024 -0.048 -0.048 -0.064M1.12 1.04h-0.08v0.08c0 0.048 -0.032 0.08 -0.08 0.08s-0.08 -0.032 -0.08 -0.08v-0.08h-0.08c-0.048 0 -0.08 -0.032 -0.08 -0.08s0.032 -0.08 0.08 -0.08h0.08v-0.08c0 -0.048 0.032 -0.08 0.08 -0.08s0.08 0.032 0.08 0.08v0.08h0.08c0.048 0 0.08 0.032 0.08 0.08s-0.032 0.08 -0.08 0.08" />
@@ -57,9 +53,7 @@ const SmallFeature = () => {
               <p className="text-gray-600 text-sm leading-relaxed">Preventive care when preventive care.</p>
             </div>
           </div>
-          <div className={`grid grid-flow-col auto-col-max transition-all duration-700 transform delay-300 ${
-            isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
-          }`}>
+          <div className={`grid grid-flow-col auto-col-max transition-all duration-700 transform delay-300 ${ isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10' }`}>
             <div className="w-16 h-16 flex justify-center">
               <svg fill="#155dfc" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" width="64px" height="64px" viewBox="0 0 1.6 1.6" enableBackground="new 0 0 20 20">
                 <path d="M1.52 0.56c-0.048 0 -0.08 -0.032 -0.08 -0.08 0 -0.176 -0.144 -0.32 -0.32 -0.32 -0.104 0 -0.192 0.048 -0.256 0.128 -0.032 0.04 -0.096 0.04 -0.128 0C0.672 0.208 0.584 0.16 0.48 0.16 0.304 0.16 0.16 0.304 0.16 0.48c0 0.048 -0.032 0.08 -0.08 0.08s-0.08 -0.032 -0.08 -0.08c0 -0.264 0.216 -0.48 0.48 -0.48 0.12 0 0.232 0.048 0.32 0.12C0.888 0.04 1 0 1.12 0c0.264 0 0.48 0.216 0.48 0.48 0 0.048 -0.032 0.08 -0.08 0.08" />
