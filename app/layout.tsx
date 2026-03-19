d6bd56c6a733c134c89936e9596711570de1851f
Mannybranch <78517183+justMeManny@users.noreply.github.com>
2026-03-19 13:20:43 +0800

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Lumina: Health Care",
